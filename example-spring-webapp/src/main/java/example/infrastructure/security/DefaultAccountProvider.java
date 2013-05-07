5779c2e1fb12e0c6727bc1f5daf4d4d614512086
Marcin Kuthan <marcin.kuthan@gmail.com>
2013-05-07 15:33:20 +0200

    Add missing spring annotations.

--- example-spring-webapp/src/main/java/example/infrastructure/security/DefaultAccountProvider.java
+++ example-spring-webapp/src/main/java/example/infrastructure/security/DefaultAccountProvider.java
@@ -1,14 +1,18 @@
 package example.infrastructure.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import example.domain.shared.security.Account;
 import example.domain.shared.security.AccountProvider;
 import example.domain.shared.security.AccountRepository;
 
+@Component
 public class DefaultAccountProvider implements AccountProvider {
 
+	@Autowired
 	private AccountRepository accountRepository;
 
 	@Override
